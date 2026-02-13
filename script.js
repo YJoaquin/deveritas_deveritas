3901d28614d393b7c578182c9aa422594c58cd76
YJoaquin <joaquin.perezcasaverde@iestpmonfortianos.edu.pe>
2026-02-13 15:59:31 -0500

    soy lo maximo

---
+++ script.js
@@ -0,0 +1,46 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+
+let scale = 1;
+
+
+function moveNoButton() {
+
+    const btnWidth = noBtn.offsetWidth;
+    const btnHeight = noBtn.offsetHeight;
+
+    const maxX = window.innerWidth - btnWidth;
+    const maxY = window.innerHeight - btnHeight;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    noBtn.style.position = "fixed";
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+
+    growYesButton();
+}
+
+
+function growYesButton() {
+    scale += 0.15;
+    yesBtn.style.transform = `scale(${scale})`;
+}
+
+
+noBtn.addEventListener("mouseenter", moveNoButton);
+
+noBtn.addEventListener("click", moveNoButton);
+
+
+yesBtn.addEventListener("click", () => {
+    window.location.href = "acepto.html";
+});
+
+
+window.addEventListener("resize", () => {
+    noBtn.style.position = "relative";
+    noBtn.style.left = "0";
+    noBtn.style.top = "0";
+});
